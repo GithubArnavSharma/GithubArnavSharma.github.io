6a8f87bf52f06d82c9f303990b124a936c0d6e5b
GithubArnavSharma <namearnavsharma@gmail.com>
2022-04-23 20:35:06 -0700

    Update mainThing.py

--- mainThing.py
+++ mainThing.py
@@ -3,7 +3,7 @@ import pickle
 import numpy as np
 import pandas as pd
 
-model = pickle.load(open('C:/Users/14086/Downloads/heart_model.sav', 'rb'))
+#model = pickle.load(open('C:/Users/14086/Downloads/heart_model.sav', 'rb'))
 conversion = {'No': 0, 'Yes': 1, 'Female': 0, 'Male': 1, '18-24': 0, '25-29': 1, '30-34': 2, '35-39': 3, '40-44': 4, '45-49': 5, '50-54': 6, '55-59': 7, '60-64': 8, '65-69': 9, '70-74': 10, '75-79': 11, '80 or older': 12, 'American Indian/Alaskan Native': 0, 'Asian': 1, 'Black': 2, 'Hispanic': 3, 'Other': 4, 'White': 5, 'Fair': 0, 'Good': 1, 'Poor': 2, 'Very good': 3}
 uniqueCol = {'HeartDisease': ['No', 'Yes'], 'BMI': [], 'Smoking': ['No', 'Yes'], 'AlcoholDrinking': ['No', 'Yes'], 'Stroke': ['No', 'Yes'], 'PhysicalHealth': [], 'MentalHealth': [], 'DiffWalking': ['No', 'Yes'], 'Sex': ['Female', 'Male'], 'AgeCategory': ['18-24', '25-29', '30-34', '35-39', '40-44', '45-49', '50-54', '55-59', '60-64', '65-69', '70-74', '75-79', '80 or older'], 'Race': ['American Indian/Alaskan Native', 'Asian', 'Black', 'Hispanic', 'Other', 'White'], 'Diabetic': ['No', 'Yes'], 'PhysicalActivity': ['No', 'Yes'], 'GenHealth': ['Fair', 'Good', 'Poor', 'Very good'], 'SleepTime': [], 'Asthma': ['No', 'Yes'], 'KidneyDisease': ['No', 'Yes'], 'SkinCancer': ['No', 'Yes']}
 
@@ -42,9 +42,9 @@ def home():
             arr.append(d)
 
     arr = np.array(numeric_arr(arr))
-    prob = prob_heart_disease(arr)
+    #prob = prob_heart_disease(arr)
 
-    return render_template('result.html', prob=prob)
+    return render_template('result.html', prob=50)
 
 if __name__ == "__main__":
     app.run(debug=True)
